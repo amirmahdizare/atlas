f5435dbba84d8180b39736b9e97df0918141fe24
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-10-31 13:36:58 +0330

    Init Login Page

---
+++ app/(general)/login/ClientPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const ClientPage = () => {
+    return (
+        <div className='container max-w-[1400px] mx-auto'>
+            <div className='grid grid-cols-6 p-2 gap-4 '>
+
+                <div className='col-span-4 flex flex-row items-center justify-center p-8'>
+
+                    <form>
+                        ثبت نام/ ورود به اطلس
+                    </form>
+                </div>
+
+                
+                <div className='col-span-2'>
+                    عکس
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
