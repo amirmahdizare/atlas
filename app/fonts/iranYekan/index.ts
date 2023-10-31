99bc8c3a93862ffa99bf0d6150aa89dcda073e50
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-10-31 12:19:42 +0330

    Add Fonts

---
+++ app/fonts/iranYekan/index.ts
@@ -0,0 +1,107 @@
+import localFont from 'next/font/local'
+
+export const IRANYekan= localFont({
+    src: [
+
+        {
+            path: './IRANYekanX-Thin.ttf',
+            weight: '200'
+        },
+        {
+            path: './IRANYekanX-UltraLight.ttf',
+            weight: '200'
+        },
+        {
+            path: './IRANYekanX-Light.ttf',
+            weight: '300'
+        },
+        {
+            path: './IRANYekanX-Regular.ttf',
+            weight: '400'
+        },
+        {
+            path: './IRANYekanX-Medium.ttf',
+            weight: '500'
+        },
+        {
+            path: './IRANYekanX-DemiBold.ttf',
+            weight: '600'
+        },
+        {
+            path: './IRANYekanX-Bold.ttf',
+            weight: '700'
+        },
+
+        {
+            path: './IRANYekanX-ExtraBold.ttf',
+            weight: '800'
+        },
+        
+        {
+            path: './IRANYekanX-Black.ttf',
+            weight: '900'
+        },
+
+
+        {
+            path: './IRANYekanX-ExtraBlack.ttf',
+            weight: '950'
+        },
+
+
+    ],
+    variable: '--font-iranyekan'
+})
+
+export const IRANYekanNumber= localFont({
+    src: [
+
+        {
+            path: './IRANYekanXFaNum-Thin.ttf',
+            weight: '200'
+        },
+        {
+            path: './IRANYekanXFaNum-UltraLight.ttf',
+            weight: '200'
+        },
+        {
+            path: './IRANYekanXFaNum-Light.ttf',
+            weight: '300'
+        },
+        {
+            path: './IRANYekanXFaNum-Regular.ttf',
+            weight: '400'
+        },
+        {
+            path: './IRANYekanXFaNum-Medium.ttf',
+            weight: '500'
+        },
+        {
+            path: './IRANYekanXFaNum-DemiBold.ttf',
+            weight: '600'
+        },
+        {
+            path: './IRANYekanXFaNum-Bold.ttf',
+            weight: '700'
+        },
+
+        {
+            path: './IRANYekanXFaNum-ExtraBold.ttf',
+            weight: '800'
+        },
+        
+        {
+            path: './IRANYekanXFaNum-Black.ttf',
+            weight: '900'
+        },
+
+
+        {
+            path: './IRANYekanXFaNum-ExtraBlack.ttf',
+            weight: '950'
+        },
+
+
+    ],
+    variable: '--font-iranyekan'
+})
