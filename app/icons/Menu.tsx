99d614b2a9c5caa010b3efc8950bd697d4a75d62
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-02 13:56:58 +0330

    Add Icon

---
+++ app/icons/Menu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Menu = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 30 26" fill="none" {...props}>
+            <path d="M1.66602 2.33301H14.9993M1.66602 12.9997H28.3327M14.9993 23.6663H28.3327" stroke="#28303F" stroke-width="3" stroke-linecap="round" />
+        </svg>
+    )
+}
