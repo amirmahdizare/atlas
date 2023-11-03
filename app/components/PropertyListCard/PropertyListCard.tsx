b519b9b2679151c2c88cf3d194b5af24d3999a89
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-03 16:05:19 +0330

    Create PropertyListCard

---
+++ app/components/PropertyListCard/PropertyListCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Image from 'next/image'
+import { PropertyListItemType } from 'types'
+import { Bookmark, BookmarkFilled } from '@icons'
+import { IconArrowDownLeft, IconLocation, IconMapPin } from '@tabler/icons-react'
+import { BookmarkStatus } from './components/BookmarkStatus'
+import Link from 'next/link'
+
+export const PropertyListCard = ({ id, img, isSaved, location, price, subLocation, title }: PropertyListItemType) => {
+
+
+    return (
+        <Link href={`/property/${id}`} className='flex flex-col p-2 bg-white items-stretch rounded-[2px] gap-2 ' prefetch={false}>
+
+            <div className='flex flex-row justify-center'>
+
+
+                <div className='relative aspect-[1.5/1] w-[170px] lg:w-[200px] rounded-sm overflow-hidden'>
+
+                    <Image src={img} alt={`${title} | دپارتمان املاک اطلس`} fill className='object-cover' />
+
+                    <div className='absolute aspect-square bg-white rounded shadow left-1 top-1 p-1'>
+                        <BookmarkStatus isSaved={isSaved} />
+                    </div>
+
+                </div>
+            </div>
+
+            {/* <div className='leading-6'> */}
+            <span className='text-raisin-black text-h5-bolder leading-3 line-clamp-2 h -6  text-ellipsis ' title={title}>{title}</span>
+            {/* </div> */}
+
+            <div className='flex flex-row gap-0.5 items-center'>
+                <IconMapPin width={20} height={20} className='text-french-gray' />
+                <span className='text-ultra-violet line-clamp-1 text-ellipsis' title={`${location} ${subLocation ? `, ${subLocation}` : ''}`}>{location} {subLocation ? `, ${subLocation}` : ''}</span>
+            </div>
+
+            <div className='flex flex-row gap-2'>
+                <p>
+                    <span className='text-space-codet text-h6-bolder'>{price.toLocaleString()}</span>
+                    <span className='text-ultra-violet text-body-2-light'> تومان</span>
+                </p>
+
+                <div className='aspect-square bg-orange-200 rounded hidden lg:block'>
+                    <IconArrowDownLeft className='text-orange' />
+                </div>
+
+            </div>
+
+        </Link>
+    )
+}
