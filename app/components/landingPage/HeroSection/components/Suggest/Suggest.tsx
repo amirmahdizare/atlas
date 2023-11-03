76352ff84a092c55e6f3f39e77fdb9a447d704f5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-03 12:48:21 +0330

    Modify Suggest

--- app/components/landingPage/HeroSection/components/Suggest/Suggest.tsx
+++ app/components/landingPage/HeroSection/components/Suggest/Suggest.tsx
@@ -10,15 +10,15 @@ export const Suggest = () => {
     return (
 
 
-        <div className='grid grid-cols-3 lg:grid-cols-5 text-h6-normal gap-1 items-center lg:items-start  px-2 lg:px-0'>
+        <div className='grid grid-cols-3 lg:grid-cols-5 text-h6-normal gap-3 lg:gap-1 items-center lg:items-start  px-2 lg:px-0'>
 
-            <div className='col-span-1 lg:col-span-1 flex flex-col gap-1.5 text-center'>
-                <span className='text-ghost-white'>AMLAKATLAS</span>
+            <div className='col-span-3 lg:col-span-1 flex flex-row lg:flex-col gap-0.5 lg:gap-1.5 text-center'>
+                <span className='text-ghost-white lg:block hidden'>AMLAKATLAS</span>
                 <span className='text-coral text-h3-bolder'>پیشنهـــاد</span>
                 <span className='text-raisin-black text-h3-bolder'>اطلــــس</span>
             </div>
 
-            <div className='flex flex-row col-span-2 lg:col-span-4 gap-2 text-white overflow-auto '>
+            <div className='flex flex-row col-span-3 lg:col-span-4 gap-2 text-white overflow-auto '>
                 {sample}
                 {sample}
                 {sample}
