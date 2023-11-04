689b87856424a8e1d748bcb2e78a3f83f430e26a
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-04 17:00:41 +0330

    Fix News

--- app/components/landingPage/News/News.tsx
+++ app/components/landingPage/News/News.tsx
@@ -6,9 +6,19 @@ import { Slider } from './components/Slider/Slider'
 export const News = () => {
     return (
         <div className='flex flex-col gap-6 items-stretch'>
-            <div className='text-raisin-black text-h3-bolder relative text-center'>
-                آگهی های جدید
-                <div className='absolute bg-mint-green w-[80px] left-1/2 -translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
+            <div className='flex flex-row gap-4 justify-between lg:justify-center'>
+
+                <div className='text-raisin-black text-h3-bolder relative text-center'>
+                    آگهی های جدید
+                    <div className='absolute bg-mint-green w-[80px] left-1/2 -translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
+                </div>
+
+
+
+                <div className='flex flex-row gap-1 cursor-pointer hover:text-coral lg:hidden'>
+                    <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
+                    <IconArrowDownLeft width={15} height={15} />
+                </div>
             </div>
 
             <div className='flex flex-col p-2 lg:p-4 bg-seasalt gap-2 overflow-hidden'>
@@ -16,27 +26,27 @@ export const News = () => {
 
                     <div className='flex flex-row gap-2 items-center'>
                         <Building width={25} height={25} className='text-raisin-black' />
-                        <p className='text-h6-bolder'>
+                        <p className='text-h6-bolder text-ellipsis line-clamp-1'>
                             <span className='text-mint-green'>فروش و رهن و اجاره</span>
                             &nbsp;
                             مسکونی، تجاری و اداری
                         </p>
 
-                        <div className='bg-anti-flash-white-lighter p-1 text-body-3-normal flex flex-row gap-1 items-center'>
+                        <div className='bg-anti-flash-white-lighter p-1 text-body-3-normal  flex-row  gap-1 items-center hidden lg:flex'>
                             <Announcement className='text-ultra-violet' width={20} height={20} />
                             <span className='text-body-3-normal'>+58 آگهی</span>
                         </div>
 
                     </div>
 
-                    <div className='flex flex-row gap-1 cursor-pointer hover:text-coral'>
+                    <div className=' flex-row gap-1 cursor-pointer hover:text-coral hidden lg:flex'>
                         <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
                         <IconArrowDownLeft width={15} height={15} />
                     </div>
 
                 </div>
 
-            <Slider />
+                <Slider />
             </div>
 
 
