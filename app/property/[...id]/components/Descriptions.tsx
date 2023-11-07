c4a51c3b3938ba17e4eac702c647199270a175b7
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-07 12:44:10 +0330

    Add Description

---
+++ app/property/[...id]/components/Descriptions.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PropertyDetailType } from 'types'
+
+export const Descriptions = ({data:{ description}}:{data: PropertyDetailType}) => {
+  return (
+    <div className='flex flex-col gap-2'>
+        <span className='text-body-1-bolder text-space-codet'>توضیحات</span>
+        <p className='text-ultra-violet leading-3'>
+           {description} 
+        </p>
+
+    </div>
+  )
+}
--- app/property/[...id]/page.tsx
+++ app/property/[...id]/page.tsx
@@ -3,12 +3,12 @@ import { pageProps } from 'types'
 import { Breadcrumb } from './components/Breadcrumb'
 import { sampleProprty } from './data.mock'
 import { MobileBreadcrumb } from './components/MobileBreadcrumb'
-import { Title } from './components'
+import { Descriptions, Title } from './components'
 
 
 export default function page({ params: { id } }: pageProps<{ id: string[] }>) {
     return (
-        <div className='grid grid-cols-5 gap-2 py-1.5 '>
+        <div className='grid grid-cols-5 gap-4 py-1.5 '>
             <div className='order-1 col-span-5 lg:block hidden'>
                 <Breadcrumb data={sampleProprty} />
             </div>
@@ -19,6 +19,11 @@ export default function page({ params: { id } }: pageProps<{ id: string[] }>) {
 
             <div className='order-2 lg:order-1 col-span-5 lg:col-span-3 '>
                 <Title data={sampleProprty} />
+            </div>
+
+            <div className='order-3 col-span-5 lg:col-span-3'>
+                <Descriptions data={sampleProprty} />
+
             </div>
 
             <div className='order-1 lg:order-2 col-span-5 lg:col-span-2'>
