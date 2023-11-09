5ec3afa015a885b479505665d40ecb47c8414b00
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-09 18:12:31 +0330

    Add Preview

---
+++ app/property/[...id]/components/Media/Media.tsx
@@ -0,0 +1,101 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
+import { PropertyDetailType } from 'types'
+import { FreeMode, Navigation, Pagination, Thumbs } from 'swiper/modules';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
+import { Thumb } from './components/Thumb';
+
+export const Media = ({ data: { medias } }: { data: PropertyDetailType }) => {
+
+    const [state, setState] = useState<boolean>(false)
+
+    const [slide, setSlide] = useState<number>(0)
+
+    const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+
+    const [isInit, setIsInit] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsInit(true)
+    }, [])
+
+
+    return (
+        <div className={`w-full ${state ? 'fixed top-0 left-0 w-full h-full backdrop-brightness-[25%] transition-all duration-300  lg:backdrop-brightness-[25%] p-1  box-border flex justify-center items-center z-30' : 'relative '}`}>
+
+            {!!slide && <div className='absolute top-1/2 right-1.5 -translate-y-1/2 cursor-pointer p-0.5 aspect-square lg:hover:bg-gray-50 rounded-circle z-10 text-gray-100 lg:text-gray-700' id='prevSlideMedia'>
+                <IconChevronRight />
+            </div>}
+
+            {slide + 1 != medias?.length && <div className='absolute top-1/2 -translate-y-1/2 left-1.5 cursor-pointer p-0.5 aspect-square hover:bg-gray-50 rounded-circle z-10 text-gray-100 lg:text-gray-700' id='nextSlideMedia'>
+                <IconChevronLeft />
+            </div>}
+
+
+
+            <Swiper
+                spaceBetween={10}
+                slidesPerView={1}
+                onSlideChange={(e) => setSlide(e.activeIndex)}
+                onSwiper={(swiper) => console.log(swiper)}
+                className='flex flex-col items-stretch justify-stretch backdrop-brigh tness-50 relative'
+                modules={[Pagination, Navigation]}
+                pagination={{
+                    enabled: true,
+                    el: '.pagination-ss',
+                    bulletActiveClass: 'active',
+                    bulletClass: 'w-2 h-1 rounded bg-gray-200'
+                    // currentClass:'bg-red-500',
+                    // dynamicMainBullets:
+                    // renderBullet:(index , className )=>{
+                    //     console.log(className)
+                    //     return `<span class="w-2 h-1 rounded  ${className =='active' ? 'bg-mint-green'  : 'bg-white'} "></span>`
+
+                    // }
+                }}
+                navigation={
+                    {
+                        enabled: true,
+                        nextEl: '#nextSlideMedia',
+                        prevEl: '#prevSlideMedia'
+                    }
+                }
+                thumbs={isInit ? { swiper: thumbsSwiper } : undefined}
+
+            >
+                <div className='pagination-ss left-4 flex flex-row gap-1 items-center justify-end -bottom-8 absolute px-1 z-10 [&_.active]:!bg-mint-green' />
+                {medias?.map(item => <SwiperSlide className='h-full'>
+                    <img onClick={() => setState(!state)} src={item} className=' rounded w-full max-h-[80vh] aspect-video  object-cover bg- seasalt b order border-anti-flash-white-lighter' />
+                </SwiperSlide>)}
+
+            </Swiper>
+
+            <Swiper
+                onSwiper={(e) => {
+                    // if (e)
+                        setThumbsSwiper(e)
+                }}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mt-1"
+            >
+                {medias?.map(item => <SwiperSlide className='h-full'>
+
+
+                    {/* <img onClick={() => setState(!state)} src={item} className='cursor-pointer border-2 duration-100 translate-all hover:border-mint-green rounded w-fukll aspect-video  object-cover bg- seasalt b order border-anti-flash-white-lighter' /> */}
+                    <Thumb src={item} onClick={() => {}} />
+
+                </SwiperSlide>)}
+                {/* {medias?.map(item => <SwiperSlide className='h-full'>
+                    <img onClick={() => setState(!state)} src={item} className=' rounded h-4 w-4 aspect-video  object-cover bg- seasalt b order border-anti-flash-white-lighter' />
+                </SwiperSlide>)} */}
+
+            </Swiper>
+
+        </div>
+    )
+}
---
+++ app/property/[...id]/components/Media/components/Thumb.tsx
@@ -0,0 +1,23 @@
+import { IconPlayerPlayFilled } from '@tabler/icons-react'
+import React from 'react'
+import { isFileSrcImage, isFileSrcVideo } from 'utils'
+
+export const Thumb = ({ src, onClick }: { src: string, onClick: Function }) => {
+
+    if (isFileSrcImage(src))
+        return <img src={src} onClick={() => onClick()} className='cursor-pointer border-2 duration-100 translate-all hover:border-mint-green rounded w-fukll aspect-video  object-cover bg- seasalt b order border-anti-flash-white-lighter' />
+
+    else if (isFileSrcVideo(src))
+
+        return <div className='rounded object-cover h-full aspect-video relative cursor-pointer '>
+            <video height="100%" onClick={() => onClick()} className='rounded object-cover h-full aspect-video border-2 duration-100 translate-all hover:border-mint-green'>
+                <source src={src} type={`video/${src.split('.').reverse()[0]}`} />
+
+            </video>
+            <IconPlayerPlayFilled width={20} height={20} className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-white' />
+        </div>
+
+    return <></>
+
+
+}
