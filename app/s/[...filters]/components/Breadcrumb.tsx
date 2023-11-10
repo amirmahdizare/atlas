4d32e8985f09598ee9ba80b31fb2287ad01900b4
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-11 00:04:14 +0330

    Add BreadCamp

---
+++ app/s/[...filters]/components/Breadcrumb.tsx
@@ -0,0 +1,29 @@
+import { IconChevronLeft } from '@tabler/icons-react'
+import React from 'react'
+import { PropertyDetailType } from 'types'
+
+export const Breadcrumb = () => {
+
+    const catName = 'مسکونی'
+    const name = 'خرید'
+    const onvan = 'هشتگرد'
+    return (
+        <div className='px-2 py-1.5 text-french-gray flex flex-row items-center gap-1 text-body-3-normal bg-seasalt'>
+
+            <span >{name}</span>
+            <IconChevronLeft />
+            <span>{catName}</span>
+            <IconChevronLeft />
+            {/* 
+            {!!subCategory && <>
+                <span>{subCategory?.name}</span>
+                <IconChevronLeft />
+
+            </>} */}
+
+            <span className='text-raisin-black text-body-3-bolder'>{onvan}</span>
+
+
+        </div>
+    )
+}
--- app/s/[...filters]/page.tsx
+++ app/s/[...filters]/page.tsx
@@ -1,10 +1,18 @@
+import { Metadata } from 'next'
 import React from 'react'
 import { pageProps } from 'types'
+import { Breadcrumb } from './components'
 
-export default function page  ({ }: pageProps<{ filters: Array<string> }, { [key: string]: any }>)  {
+
+export const metadata: Metadata = {
+    title: 'جستجوی ملک در دپارتمان املاک اطلس'
+}
+
+export default function page({ params: { filters } }: pageProps<{ filters: Array<string> }, { [key: string]: any }>) {
 
     return (
-        <div>
+        <div className='flex flex-col gap-2'>
+            <Breadcrumb/>
             به صفحه جستجو خوش امدید
         </div>
     )
