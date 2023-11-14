060547198d410eb67307521bbd19916b8423f5a0
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-14 19:58:11 +0330

    Fix Category

--- app/s/[...filters]/components/Filters/components/CategoryFilter/CategoryFilter.tsx
+++ app/s/[...filters]/components/Filters/components/CategoryFilter/CategoryFilter.tsx
@@ -19,14 +19,19 @@ export const CategoryFilter = () => {
                 {/* <IconChevronUp width={15} height={15} /> */}
             </div>
 
-            {filter.category && <span className='text-ultra-violet text-body-3-light cursor-pointer hover:text-coral flex flex-row gap-0.5 items-center' onClick={()=>dispatchFilter({category:undefined})}>
+            {filter.category && <span className='text-ultra-violet text-body-3-light cursor-pointer hover:text-coral flex flex-row gap-0.5 items-center' onClick={() => dispatchFilter({ category: undefined })}>
                 <IconArrowRight width={15} />
                 <span>
                     همه آگهی ها
                 </span>
             </span>}
 
-            <div className='flex flex-col gap-1 items-start '>
+
+            {!filter.category && <div className={`px-1 py-0.5 transition-all duration-150 border-r-2 text-body-3-bolder cursor-pointer  ${!filter.category ? 'border-r-robin-egg-blue-00  text-robin-egg-blue-00 font-bold' : 'text-ultra-violet hover:text-coral border-r-white'}`}>
+                همه آگهی ها
+            </div>}
+            
+            <div className='flex flex-col gap-1  items-start pr-1'>
 
 
                 {categories?.map(item => {
@@ -37,13 +42,13 @@ export const CategoryFilter = () => {
                 }
 
                 )}
-            </div>
-
-            <div className='flex flex-col gap-1 items-start pr-3'>
+            <div className='flex flex-col gap-1 items-start pr-2'>
                 {categories.find((i, index, arr) => i.id == filter.category || i.subCategories.find(i => i.id == filter.category))?.subCategories?.map(item => <div onClick={() => dispatchFilter({ category: item.id })} className={`px-1 py-1 transition-all duration-150 border-r-2 text-body-3-bolder cursor-pointer  ${filter.category == item.id ? 'border-r-robin-egg  text-robin-egg font-bold' : 'text-ultra-violet hover:text-coral border-r-white'}`}>
                     <span>{item.title}</span>
                 </div>)}
             </div>
+            </div>
+
 
 
 
--- app/s/[...filters]/components/Filters/components/SelectType/SelectType.tsx
+++ app/s/[...filters]/components/Filters/components/SelectType/SelectType.tsx
@@ -8,8 +8,8 @@ export const SelectType = () => {
     
     return (
         <div className='flex flex-row gap-0.5 text-body-3-normal bo rder  rounded borde r-mint-green'>
-            <button className={`${filter.type=='SELL' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'SELL'})}>فروش</button>
-            <button className={`${filter.type=='RENT' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'RENT'})}>رهن و اجاره</button>
+            <button className={`${filter.type=='SELL' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'SELL'})}>فروش</button>
+            <button className={`${filter.type=='RENT' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'RENT'})}>رهن و اجاره</button>
         </div>
     )
 }
