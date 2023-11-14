77ac2370042206acb1a2a13d3d852c92ecd62262
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-11-14 20:01:17 +0330

    Add Title

--- app/s/[...filters]/components/Filters/Filters.tsx
+++ app/s/[...filters]/components/Filters/Filters.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { CategoryFilter, SelectType } from './components'
 
+const Divider = () => <div className='bg-anti-flash-white-lighter w-full h-[1px]'></div>
+
 export const Filters = () => {
   return (
-    <div className='flex flex-col gap-3'>
+    <div className='flex flex-col gap-2'>
         <SelectType />
-
+        <Divider/>
         <CategoryFilter />
+        <Divider/>
 
 
     </div>
--- app/s/[...filters]/components/Filters/components/SelectType/SelectType.tsx
+++ app/s/[...filters]/components/Filters/components/SelectType/SelectType.tsx
@@ -4,12 +4,16 @@ import { useSearchProperty } from 's/[...filters]/hooks'
 
 export const SelectType = () => {
 
-    const {filter , dispatchFilter} = useSearchProperty()
-    
+    const { filter, dispatchFilter } = useSearchProperty()
+
     return (
-        <div className='flex flex-row gap-0.5 text-body-3-normal bo rder  rounded borde r-mint-green'>
-            <button className={`${filter.type=='SELL' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'SELL'})}>فروش</button>
-            <button className={`${filter.type=='RENT' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={()=>dispatchFilter({type:'RENT'})}>رهن و اجاره</button>
+        <div className='flex flex-col gap-2'>
+            <span className='text-space-codet text-body-3-bolder'>نوع آگهی</span>
+
+            <div className='flex flex-row gap-0.5 text-body-3-normal bo rder  rounded borde r-mint-green'>
+                <button className={`${filter.type == 'SELL' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={() => dispatchFilter({ type: 'SELL' })}>فروش</button>
+                <button className={`${filter.type == 'RENT' ? 'bg-mint-green text-white' : 'bg-white hover:bg-gray-100 border border-gray-100'} flex-1 p-1 rounded transition-all dur`} onClick={() => dispatchFilter({ type: 'RENT' })}>رهن و اجاره</button>
+            </div>
         </div>
     )
 }
