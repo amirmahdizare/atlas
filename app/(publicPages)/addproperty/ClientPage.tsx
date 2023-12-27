d32a29bedf4ba43f372008c41caad35f6fe560ed
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-12-27 19:19:47 +0330

    Add Select To ClientPage

--- app/(publicPages)/addproperty/ClientPage.tsx
+++ app/(publicPages)/addproperty/ClientPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { Button, Input, TextArea } from '@components'
+import { Button, Input, Select, TextArea } from '@components'
 import Image from 'next/image'
 import upload from 'images/upload.svg'
 import vector from 'images/housesVector.svg'
@@ -26,7 +26,63 @@ export const ClientPage = () => {
 
                 <Input placeholder='مثلا :  علی رضایی' label='نام و نام خانوادگی' />
 
-                <TextArea label='توضیحات' placeholder='مثلا : آپارتمان 200 متری دارای پارکینگ و آسانسور و ...'/>
+                <TextArea label='توضیحات' placeholder='مثلا : آپارتمان 200 متری دارای پارکینگ و آسانسور و ...' />
+
+                <div className='grid grid-cols-2 gap-2'>
+
+                    <div className='col-span-2 lg:col-span-1'>
+
+                        <Select
+                            items={[{ value: 'hashtgerd', lable: 'هشتگرد' }, { lable: 'شهر جدید هشتگرد', value: 'newCirty' }]}
+                            onChange={() => { }}
+                            value={'hashtgerd'}
+                            placeHolder='انتخاب شهر'
+                            label='شهر'
+
+                        />
+                    </div>
+
+                    <div className='col-span-2 lg:col-span-1'>
+
+                        <Select
+                            items={[{ value: 'hashtgerd', lable: 'فاز 7 ' }, { lable: 'فاز 4', value: 'newCirty' }]}
+                            onChange={() => { }}
+                            value={'hashtgerd'}
+                            placeHolder='انتخاب منظقه'
+                            label='منطقه'
+
+                        />
+                    </div>
+
+                </div>
+
+                <div className='grid grid-cols-2 gap-2'>
+
+                    <div className='col-span-2 lg:col-span-1'>
+
+                        <Select
+                            items={[{ value: 'hashtgerd', lable: 'باغ' }, { lable: 'زمین', value: 'newCirty' }]}
+                            onChange={() => { }}
+                            value={'hashtgerd'}
+                            placeHolder='انتخاب دسته بندی'
+                            label='دسته بندی'
+
+                        />
+                    </div>
+
+                    <div className='col-span-2 lg:col-span-1'>
+
+                        <Select
+                            items={[{ value: 'hashtgerd', lable: 'مسکونی' }, { lable: 'اداری', value: 'newCirty' }]}
+                            onChange={() => { }}
+                            value={'hashtgerd'}
+                            placeHolder='انتخاب زیردسته بندی'
+                            label='زیر دسته بندی'
+
+                        />
+                    </div>
+
+                </div>
 
 
                 <div className='flex flex-row p-4 gap-4 items-center justify-between bg-seasalt border-ghost-white border-dashed'>
