b755d76da147f01f9ef389556b45cd5b5a69d732
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-12-28 13:57:04 +0330

    Add Propert Card List

--- app/(panel)/panel/components/PropertyManagement/components/List/List.tsx
+++ app/(panel)/panel/components/PropertyManagement/components/List/List.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { news } from './data.mock'
+import { PropertyCard } from './components/PropertyCard'
 
 export const List = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
-            {[1, 2, 2, 2,].map(item => <div className='bg-white border p-2'>
-                {item}
+            {news.map(item => <div className='bg-white shadow rounded p-1 hover:bg-gray-50'>
+                <PropertyCard {...item} />
             </div>)}
         </div>
     )
---
+++ app/(panel)/panel/components/PropertyManagement/components/List/data.mock.ts
@@ -0,0 +1,91 @@
+import { PropertyListItemType } from "types";
+
+export const news: PropertyListItemType[] = [
+    {
+        id: 'sdfsdf',
+        img: 'https://www.khanetarh.com/wp-content/uploads/2019/06/The-most-beautiful-houses-in-the-world-1.webp',
+        isSaved: false,
+        location: 'شهرجدید هشتگرد',
+        price: 5400000000,
+        subLocation: 'فاز1',
+        title: 'آپارتمان  نوساز 115 متری کلید تحویل',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    },
+    {
+        id: 'sdfsdfsdf',
+        img: 'https://arga-mag.com/file/img/2020/03/Great-home-decoration.jpg',
+        isSaved: false,
+        location: 'هشتگرد',
+        price: 5600000000,
+        subLocation: 'فاز2',
+        title: 'آپارتمان  نوساز 95 متری کلید تحویل',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    },
+    {
+        id: 'sdfsdfsdf',
+        img: 'https://www.beautyhome.ir/wp-content/uploads/2015/09/Modern-home-interior-decoration-61.jpg',
+        isSaved: false,
+        location: 'شهرجدید هشتگرد',
+        price: 7000000000,
+        subLocation: 'فاز7',
+        title: 'ویلایی کلنگی 200 متری',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    },
+    {
+        id: 'sdfds',
+        img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp',
+        isSaved: false,
+        location: 'تهران دشت',
+        price: 100000000,
+        title: 'ویلا دوبلکس',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    },
+    {
+        id: '1234',
+        img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp',
+        isSaved: true,
+        location: 'کردان',
+        price: 100000000,
+        title: 'ویلا کردان مناسب بازسازی',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    },
+    {
+        id: 'sdf454',
+        img: 'https://cdn.downloadefilm.ir/images/29b04000-596d-11ee-b57a-a5d77f02b41c.jpg',
+        isSaved: true,
+        location: 'لواسان',
+        price: 23000000000,
+        title: 'آپارتمان دوبلکس',
+        agentInfo: {
+            name: 'امیرحسین کشن زارع',
+            avatar: '',
+            phoneNumber: '09196442715',
+            username: 'amir24'
+        }
+    }
+]
