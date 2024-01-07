de2488e65efa9a394194d64977a9ea0448d363b3
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-01-07 20:22:10 +0330

    Fix

--- app/(publicPages)/a/[id]/components/News/components/Properties.tsx
+++ app/(publicPages)/a/[id]/components/News/components/Properties.tsx
@@ -6,6 +6,10 @@ import { news } from '../data.mock';
 import { PropertyListCard } from '@components';
 
 export const Properties = () => {
+
+    if (news.length == 0)
+        return <span className='text-center'>مشاور آگهی فعالی ندارد</span>
+
     return (
         <div className='grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-1'>
 
