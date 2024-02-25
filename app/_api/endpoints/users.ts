9a438a4cfbc8f08f0f17f0a1ec0047cbb78bfe5b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-25 19:01:01 +0330

    Add Create User Endpoint

--- app/_api/endpoints/users.ts
+++ app/_api/endpoints/users.ts
@@ -1,10 +1,18 @@
-import { ApiGetRequestType, UserType } from "types"
+import { ApiGetRequestType, ApiPostRequestType, UserType } from "types"
 
 export const AuthEndpoints = Object.freeze({
     GET_USERS: '/users',
+    CREATE_USER: '/users/create'
 })
 
 
 export interface UsersEndpointType {
-    GET_USERS: ApiGetRequestType<{}, Array<UserType>>
+    GET_USERS: ApiGetRequestType<{}, Array<UserType>>,
+    CREATE_USER: ApiPostRequestType<{ firstName: string, lastName: string, phoneNumber: string }, {
+        firstName: string,
+        lastName: string,
+        phoneNumber: string,
+        role: null | { id: number, name: string },
+        id: number
+    }>
 }
