d7d16d9912f630b10d0c7a4512446d6c949dadf5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-25 19:08:14 +0330

    Add Get User Role API

--- app/_api/endpoints/roles.ts
+++ app/_api/endpoints/roles.ts
@@ -1,9 +1,11 @@
-import { ApiPostRequestType } from "types"
+import { ApiGetRequestType, ApiPostRequestType } from "types"
 
 export const RoleEndPoints = Object.freeze({
     CREATE_ROLE: '/roles/create', ///SuperAdmin , Admin , Agent , User
+    GET_ROLE_INFO: '/roles'
 })
 
 export interface RoleEndPointsType {
-    CREATE_ROLE: ApiPostRequestType<{ name: string }, { name: string, id: string }>
+    CREATE_ROLE: ApiPostRequestType<{ name: string }, { name: string, id: string }>,
+    GET_ROLE_INFO: ApiGetRequestType<{}, { id: number, name: string }>
 }
