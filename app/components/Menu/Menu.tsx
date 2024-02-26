84a7a760023802395bd084ac1f0a8bd03cf51c7b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-26 20:23:12 +0330

    Fix Menu

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -9,6 +9,8 @@ export const Menu = ({onClickLink}:{onClickLink?:Function}) => {
         <Link className='hover:text-coral' href={'/login'}>ورود / ثبت نام</Link>
         <Link className='hover:text-coral' href={'/addproperty'}>فروش ملک</Link>
         <Link className='hover:text-coral' href={'/requestproperty'}>درخواست ملک</Link>
+        <Link className='hover:text-coral' href={'/aboutus'}>درباره ما</Link>
+        <Link className='hover:text-coral' href={'/contactus'}>تماس با ما</Link>
 
 
     </div>
