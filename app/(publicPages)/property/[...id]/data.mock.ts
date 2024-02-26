9d376f968069828cab51a709c4cdc10ca76b595d
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-26 19:39:48 +0330

    Fix Image URL

--- app/(publicPages)/property/[...id]/data.mock.ts
+++ app/(publicPages)/property/[...id]/data.mock.ts
@@ -30,7 +30,7 @@ export const sampleProprty: PropertyDetailType = {
     medias: [
 
         "https://www.otag.ir/wp-content/uploads/%D8%B9%DA%A9%D8%B3-%D8%AF%D8%A7%D8%AE%D9%84-%D8%A2%D9%BE%D8%A7%D8%B1%D8%AA%D9%85%D8%A7%D9%86-19.jpg",
-        "https://cdn.nody.ir/files/2021/09/21/nody-%D8%B9%DA%A9%D8%B3-%D8%AF%D8%A7%D8%AE%D9%84-%D8%A2%D9%BE%D8%A7%D8%B1%D8%AA%D9%85%D8%A7%D9%86-%D8%A7%DB%8C%D8%B1%D8%A7%D9%86%DB%8C-1632176481.jpg",
+        "https://www.tasvirezendegi.com/wp-content/uploads/2014/10/1524871_385830448238883_7611691258591111749_n.jpg",
        "https://www.beautyhome.ir/wp-content/uploads/2019/02/Interior-design-of-the-house-850x491.jpg",
         "https://s100.divarcdn.com/static/videos/aaf13b4a-ef3d-4d4d-9cb0-9c2b75ac92c5.mp4",
     ],
