f2d6e81056d1a14dc26287b0f2054dcb2f8339c6
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-26 20:20:08 +0330

    Change View

--- app/(publicPages)/s/[...filters]/components/List/List.tsx
+++ app/(publicPages)/s/[...filters]/components/List/List.tsx
@@ -4,7 +4,7 @@ import { PropertyListCard } from '../PropertyListCard/PropertyListCard'
 
 export const List = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1'>
+        <div className='grid grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1'>
             {properties.map(item => <div className='col-span-1'>
                 <PropertyListCard {...item} />
             </div>)}
