88f3242003e4322a55e63560cf3aad8280d98b46
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-08 15:07:24 +0330

    Config Api

--- app/_api/config.ts
+++ app/_api/config.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { getToken } from "utils";
 
 export const api = axios.create({
     baseURL:process.env.NEXT_PUBLIC_API,
     headers:{
-        Authorization:`bearer ${'--'}`
+        Authorization:`bearer ${getToken()}`
     }
 })
