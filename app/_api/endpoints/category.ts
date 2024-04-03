4feaa4cb17dc1d5cc9d4e0c07929e76cf9d36146
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-04-03 19:05:49 +0330

    Add Full Relation Endpoints

--- app/_api/endpoints/category.ts
+++ app/_api/endpoints/category.ts
@@ -3,12 +3,16 @@ import { ApiGetRequestType, ApiPostRequestType, CategoryType_API, RoleType } fro
 export const CategoryEndPoints = Object.freeze({
     CREATE: '/category/create', ///SuperAdmin , Admin , Agent , User
     LIST: '/category',
-    SINGLE: (catId: string) => `/category/${catId}`
+    SINGLE: (catId: string) => `/category/${catId}`,
+    SINGLE_FULL: (catId: string) => `/category/${catId}/with-relations`,
+    ALL_WITH_RELATION: (catId: string) => `/category/relations`,
 })
 
 export interface CategoryEndPointsType {
     CREATE: ApiPostRequestType<CategoryType_API<undefined>, CategoryType_API<string>>,
-    LIST: ApiGetRequestType< {},CategoryType_API<string>[]>
+    LIST: ApiGetRequestType<{}, CategoryType_API<string>[]>
     UPDATE_SINGLE: ApiGetRequestType<CategoryType_API<string>, CategoryType_API<string>>,
-    DELETE_SINGLE : ApiPostRequestType<{}>
+    DELETE_SINGLE: ApiPostRequestType<{}>
+    SINGLE_FULL: ApiGetRequestType<{}>,
+    ALL_WITH_RELATION: ApiGetRequestType<{}>,
 }
--- app/_api/endpoints/subcategory.ts
+++ app/_api/endpoints/subcategory.ts
@@ -3,12 +3,16 @@ import { ApiGetRequestType, ApiPostRequestType, CategoryType_API, SubCategoryTyp
 export const SubcategoryEndPoints = Object.freeze({
     CREATE: '/sub-categories/create', 
     LIST: '/sub-categories',
-    SINGLE: (id: string) => `/sub-categories/${id}`
+    SINGLE: (id: string) => `/sub-categories/${id}`,
+    SINGLE_FULL: (catId: string) => `/sub-categories/${catId}/with-relations`,
+    ALL_WITH_RELATION: (catId: string) => `/sub-categories/relations`,
 })
 
 export interface SubcategoryEndPointsType {
     CREATE: ApiPostRequestType<SubCategoryType<undefined, string>, SubCategoryType<string, string>>,
     LIST: ApiGetRequestType<{}, SubCategoryType<string, string>[]>
     UPDATE_SINGLE: ApiGetRequestType<SubCategoryType<undefined, string>, SubCategoryType<string, string>>,
-    DELETE_SINGLE: ApiPostRequestType<{}>
+    DELETE_SINGLE: ApiPostRequestType<{}>,
+    SINGLE_FULL: ApiGetRequestType<{}>,
+    ALL_WITH_RELATION: ApiGetRequestType<{}>,
 }
