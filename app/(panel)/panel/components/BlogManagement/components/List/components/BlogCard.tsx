6afa87f80ec15f3cd7dbbcf8f41b760d7a51f5ab
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-04-07 17:48:56 +0330

    Implement Blog List

--- app/(panel)/panel/components/BlogManagement/components/List/components/BlogCard.tsx
+++ app/(panel)/panel/components/BlogManagement/components/List/components/BlogCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button } from '@components'
 import { IconPencil, IconTrash } from '@tabler/icons-react'
-import { BlogItemType } from 'types'
+import { BlogItemType, BlogItemTypeAPI } from 'types'
 import { useBlogsSection } from '../../../hooks'
 
-export const BlogCard = ({ title, createdAt, id, summary, img }: BlogItemType) => {
+export const BlogCard = ({ title,description , duration , images  , id, summary,  }: BlogItemTypeAPI<string , string>) => {
 
     const { dispatch } = useBlogsSection()
 
@@ -12,7 +12,7 @@ export const BlogCard = ({ title, createdAt, id, summary, img }: BlogItemType) =
         <div className='grid grid-cols-2 lg:grid-cols-4 gap-2 border-b pb-2'>
 
             <div className='col-span-1'>
-                <img src={img} className='rounded object-cover' />
+                <img src={images[0]} className='rounded object-cover' />
             </div>
 
             <div className='flex flex-col gap-2  col-span-1 lg:col-span-2'>
@@ -20,7 +20,7 @@ export const BlogCard = ({ title, createdAt, id, summary, img }: BlogItemType) =
 
                 <span className='text-body-3-normal text-ultra-violet line-clamp-2 text-ellipsis overflow-hidden leading-3'>{summary}</span>
 
-                <span className='text-ultra-violet'>{(new Date(createdAt)).toLocaleString('fa-ir')}</span>
+                {/* <span className='text-ultra-violet'>{(new Date(createdAt)).toLocaleString('fa-ir')}</span> */}
             </div>
 
             <div className='col-span-2 lg:col-span-1 flex flex-row items-start justify-evenly'>
