7b2913190247f9301b7c11eaddf8c43cd3f719d7
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-04-14 13:45:14 +0330

    Modify Modal

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -15,7 +15,7 @@ export const Modal = ({ open, setOpen, children, fitWidth, fitHeight }: IProps) 
 
     if (open)
         return (
-            <div className='fixed h-full w-full z-20 backdrop-blur backdrop-brightness-90  top-0 left-0 rounded   flex flex-row justify-center items-center'>
+            <div className='fixed h-full w-full z-30 backdrop-blur backdrop-brightness-90  top-0 left-0 rounded   flex flex-row justify-center items-center'>
                 <ClickAwayListener onClickAway={() => setOpen(false)}>
                     <div className={`z-10  lg:rounded-sm bg-white shadow p-1 overflow-auto ${fitHeight ? '' : 'lg:h-[660px] h-full'}  ${fitWidth ? '' : 'lg:w-[360px] w-full'}`}>
                         {children}
