697e3ab2d5b6e73dbddb876a0c7f839a8138972b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-02 20:42:44 +0330

    Change Alt

--- app/(publicPages)/property/[...id]/components/AgentInfo/components/ActionButton.tsx
+++ app/(publicPages)/property/[...id]/components/AgentInfo/components/ActionButton.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 export const ActionButton = ({ phoneNumber }: { phoneNumber: string }) => {
     return (
         <div className='flex flex-row gap-1 items-center'>
-            <Link href={`sms:+${phoneNumber}?&body=سلام وقت بخیر امکان بازدید وجود داره؟`}> <Button icon={IconMessage} bgColor='white' textColor='textGray' ></Button></Link>
+            <Link href={`sms:+${phoneNumber}?&body=سلام وقت بخیر امکان بازدید وجود داره؟`}  className='lg:hidden'> <span className='sr-only'>پیامک به مشاور</span> <Button icon={IconMessage} bgColor='white' textColor='textGray' ></Button></Link>
             <Link href={`tel:${phoneNumber}`}><Button icon={IconPhone} bgColor='primaryNormal'>تماس با مشاور</Button></Link>
         </div>
 
