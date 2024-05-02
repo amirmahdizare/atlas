59a40d0cac905a2a4bcc1e13a70096ba858662cc
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-02 19:54:39 +0330

    Add Title

--- app/(publicPages)/property/[...id]/components/Title.tsx
+++ app/(publicPages)/property/[...id]/components/Title.tsx
@@ -4,7 +4,40 @@ import { Share } from './Share'
 import { Bookmark } from './Bookmark'
 import { IconMap, IconMapPin } from '@tabler/icons-react'
 
-export const Title = ({ data: { title, id, isBookmarked, location, subLocation, price  , metr} }: { data: PropertyDetailType }) => {
+
+
+const PriceComponent = ({ primary, secondary }: { primary: { title: string, value: number }, secondary: { title: string, value: number, replacer?: string } }) => {
+    return <>
+        <div className='flex flex-row gap-2 items-center text-space-codet justify-between mt-1.5'>
+            <span className='text-body-1-bolder'>{primary.title}</span>
+
+            <div className='flex flex-row gap-1 items-center'>
+                <span className='text-h3-bolder'>{primary?.value?.toLocaleString()}</span>
+                <span className='text-ultra-violet text-body-1-bolder' >تومان</span>
+            </div>
+
+        </div>
+
+        <div className='bg-anti-flash-white-lighter w-full h-[1px]'>
+
+        </div>
+
+
+        <div className='flex flex-row gap-2 items-center text-ultra-violet justify-between '>
+            {secondary.replacer ? <span className='text-body-2-normal'>{secondary.replacer}</span> : <> <span className='text-body-2-normal'>{secondary.title}</span>
+
+                <div className='flex flex-row gap-1 items-center'>
+                    {/* <span className='text-h6-normal'>{(Math.floor(Number(price ?? 0) / metr )).toLocaleString()}</span> */}
+                    <span className='text-h6-normal'>{(secondary.value).toLocaleString()}</span>
+                    <span className='text-ultra-violet text-body-2-normal' >تومان</span>
+                </div></>}
+
+        </div>
+    </>
+}
+
+
+export const Title = ({ data: { title, id, isBookmarked, location, subLocation, price, prePrice, rentPrice, metr, productType }, }: { data: PropertyDetailType }) => {
     return (
         <div className='flex flex-col  gap-2  '>
 
@@ -26,30 +59,8 @@ export const Title = ({ data: { title, id, isBookmarked, location, subLocation, 
 
             </div>
 
-            <div className='flex flex-row gap-2 items-center text-space-codet justify-between mt-1.5'>
-                <span className='text-body-1-bolder'>قیمت کل</span>
-
-                <div className='flex flex-row gap-1 items-center'>
-                    <span className='text-h3-bolder'>{Number(price ?? 0).toLocaleString()}</span>
-                    <span className='text-ultra-violet text-body-1-bolder' >تومان</span>
-                </div>
-
-            </div>
-
-            <div className='bg-anti-flash-white-lighter w-full h-[1px]'>
-
-            </div>
-
-
-            <div className='flex flex-row gap-2 items-center text-ultra-violet justify-between '>
-                <span className='text-body-2-normal'>قیمت هرمتر</span>
-
-                <div className='flex flex-row gap-1 items-center'>
-                    <span className='text-h6-normal'>{(Math.floor(Number(price ?? 0) / metr )).toLocaleString()}</span>
-                    <span className='text-ultra-violet text-body-2-normal' >تومان</span>
-                </div>
-
-            </div>
+            {productType == 'sell' && <PriceComponent primary={{ title: 'قیمت کل', value: Number(price ?? 0) }} secondary={{ title: 'قیمت هر متر', value: Math.floor(Number(price ?? 0) / metr) }} />}
+            {productType == 'rent' && <PriceComponent primary={{ title: 'ودیعه', value: Number(prePrice ?? 0) }} secondary={{ title: 'اجاره', value: Number(rentPrice ?? 0), replacer: Number(rentPrice) == 0 ? 'رهن کامل' : undefined }} />}
 
         </div>
     )
