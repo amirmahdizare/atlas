3f24505c03402ea11d87de933e3a7a3c37173860
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-02 18:29:20 +0330

    Change React Query Issue

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -5,16 +5,20 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient({
-    defaultOptions:{queries :{
-        refetchInterval:false,
-        refetchOnWindowFocus:false
-    }}
+  defaultOptions: {
+    queries: {
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+      retryDelay: 1000 * 1 * 60
+    }
+  }
 });
 
-export const ClientLayout = ({children}:{children:ReactNode}) => {
+export const ClientLayout = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-    <ToastContainer theme='colored'/>
-    {children}</QueryClientProvider>
+      <ToastContainer theme='colored' />
+      {children}</QueryClientProvider>
   )
 }
