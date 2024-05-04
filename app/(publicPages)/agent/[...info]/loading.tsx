acaad9cfef4f7ad148644e1ac496f8214acee32b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-04 20:18:37 +0330

    Add Agent Page Skeleton

---
+++ app/(publicPages)/agent/[...info]/loading.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+        <div className='flex flex-col gap-2 px-2'>
+
+            <div className='bg-gray-100 h-24 w-full rounded animate-pulse'>
+
+            </div>
+
+
+
+            <div className='bg-gray-100 h-24 w-full rounded animate-pulse'>
+
+            </div>
+
+
+
+
+            <div className='bg-gray-100 h-24 w-full rounded animate-pulse'>
+
+            </div>
+
+
+
+        </div>
+    )
+}
