1241d3eacfc89b93f254c504b48c83f390f4494a
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-04 20:14:10 +0330

    Fix Agent Page

--- app/(publicPages)/agent/[...info]/components/News/News.tsx
+++ app/(publicPages)/agent/[...info]/components/News/News.tsx
@@ -5,26 +5,28 @@ import { Properties } from './components/Properties'
 import Link from 'next/link'
 import { UserFullInfo } from 'types'
 
-export const News = ({data}:{data:UserFullInfo}) => {
-    return (
-        <div className='flex flex-col gap-4 items-stretch py-2'>
-            <div className='flex flex-row gap-4 justify-between lg:justify-center'>
+export const News = ({ data }: { data: UserFullInfo }) => {
 
-                <div className='text-raisin-black text-h3-bolder relative text-center'>
-                    آگهی های مشاور
-                    <div className='absolute bg-mint-green w-[80px] right-0 lg:left-1/2 lg:-translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
+    if (data?.products?.length > 0)
+        return (
+            <div className='flex flex-col gap-4 items-stretch py-2'>
+                <div className='flex flex-row gap-4 justify-between lg:justify-center'>
+
+                    <div className='text-raisin-black text-h3-bolder relative text-center'>
+                        آگهی های مشاور
+                        <div className='absolute bg-mint-green w-[80px] right-0 lg:left-1/2 lg:-translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
+                    </div>
+
+
+
+                    <Link href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'} className='flex flex-row gap-1 cursor-pointer hover:text-coral lg:hidden'>
+                        <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
+                        <IconArrowDownLeft width={15} height={15} />
+                    </Link>
                 </div>
 
-
-
-                <Link href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'} className='flex flex-row gap-1 cursor-pointer hover:text-coral lg:hidden'>
-                    <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
-                    <IconArrowDownLeft width={15} height={15} />
-                </Link>
-            </div>
-
-            <div className='flex flex-col p-1 bg-seasalt gap-2 overflow-hidden'>
-                {/* <div className='flex flex-row gap-2 justify-between items-center'>
+                <div className='flex flex-col p-1 bg-seasalt gap-2 overflow-hidden'>
+                    {/* <div className='flex flex-row gap-2 justify-between items-center'>
 
                     <div className='flex flex-row gap-2 items-center'>
                         <Building width={25} height={25} className='text-raisin-black' />
@@ -48,12 +50,14 @@ export const News = ({data}:{data:UserFullInfo}) => {
 
                 </div> */}
 
-                {/* <Slider /> */}
-                <Properties data={data}/>
+                    {/* <Slider /> */}
+                    <Properties data={data} />
+                </div>
+
+
+
             </div>
+        )
 
-
-
-        </div>
-    )
+    return <></>
 }
