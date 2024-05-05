4bf1b987b3bbeff17097e12206d28d7db963257e
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-05 19:01:33 +0330

    Add Not Found Page

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+import { Button } from '@components'
+import Link from 'next/link'
+import Layout from './layout'
+import logo from 'images/logo-full.svg'
+import Image from 'next/image'
+
+export default function notfound() {
+    return (
+        <div className='flex flex-col gap-4 h-full justify-center items-center'>
+
+
+            <Image src={logo} alt='لوگوی اطلس' className='max- w-16' />
+
+            <span className='text-bittersweet text-h5-bolder'>صفحه مورد نظر یافت نشد!</span>
+
+            <Link href={'/'}><Button>بازگشت به صفحه نخست</Button></Link>
+
+        </div>
+    )
+}
