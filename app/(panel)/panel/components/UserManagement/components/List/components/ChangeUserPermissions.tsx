3324a18222932747508a3d1a26aff7e53ef9bf6a
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-21 16:55:56 +0330

    Change Permissions

--- app/(panel)/panel/components/UserManagement/components/List/components/ChangeUserPermissions.tsx
+++ app/(panel)/panel/components/UserManagement/components/List/components/ChangeUserPermissions.tsx
@@ -11,6 +11,7 @@ import { captilizeFirstLetter } from 'utils'
 import { useUserList } from '../../../hooks'
 import { PermissionEndPoints, PermissionEndPointsType } from '_api/endpoints/permission'
 import { useFieldArray, useForm } from 'react-hook-form'
+import { PermissionType } from 'types'
 
 
 // type FormValues = { permissions: string[] }
@@ -37,6 +38,8 @@ export const ChangeUserPermissions = ({ userId, userRoleId }: { userId: number, 
         staleTime: 1000 * 60 * 30
     })
 
+
+    const currentSelected = watch()
     const { isLoading: loadingUserInfo, data: userInfo, isError, refetch: fetchUserInfo } = useCustomQuery<UsersEndpointType['GET_SINGLE']>({
         queryKey: ['getUserInfo', userId.toString()],
         queryFn: ({ queryKey }) => api.get(UsersEndpoints.SINGLE_USER(userId.toString())),
@@ -46,7 +49,7 @@ export const ChangeUserPermissions = ({ userId, userRoleId }: { userId: number, 
                 reset(currentPermissionObj)
             }
         },
-        enabled:false
+        enabled: false
     })
 
     const { refetch } = useUserList()
@@ -81,16 +84,43 @@ export const ChangeUserPermissions = ({ userId, userRoleId }: { userId: number, 
             reset(data?.data.reduce((pv, cv) => ({ ...pv, [cv.id]: true }), {}))
     }
 
+    const proccessedData = data?.data.reduce<Array<{ title: string, items: PermissionType<string>[] }>>((pv, cv, index, array) => {
+        if (pv.findIndex(i => i.title == cv.action.split('_')[1]) == -1)
+            pv.push({ title: cv.action.split('_')[1], items: array.filter(i => i.action.includes(cv.action.split('_')[1])) })
+        return pv
+    }, [])
+
+
+    console.log(proccessedData)
+
+
+    const handleToggleCatSelect = (catName:string , state:boolean) =>{
+
+        reset( {...currentSelected ,  ...data?.data.filter(i=>i.action.split('_')[1]==catName).reduce((pv, cv) => ({ ...pv, [cv.id]: state }), {})})
+
+    }
+
+
 
     const Content = () => {
 
         if (isError)
             return <span className='text-red-500 text-center w-full'>خطا در دریافت اطلاعات کاربر</span>
 
-        else if (userInfo?.data)
-            return <form className='flex flex-col gap-2 p-2 max-h-[80vh] overflow-auto w-full min-w-[80vw]' onSubmit={handleSubmit(handleSubmitPermissions)}>
+        else if (userInfo?.data) {
 
-                <div className='flex flex-row justify-between items-center'>
+
+            const proccessedData = data?.data.reduce<Array<{ title: string, items: PermissionType<string>[] }>>((pv, cv, index, array) => {
+                if (pv.findIndex(i => i.title == cv.action.split('_')[1]) == -1)
+                    pv.push({ title: cv.action.split('_')[1], items: array.filter(i => i.action.split('_')[1] == cv.action.split('_')[1]) })
+                return pv
+            }, [])
+
+
+
+            return <form className=' grid grid-cols-4 gap-2 p-2 max-h-[80vh] overflow-auto w-full min-w-[90vw]' onSubmit={handleSubmit(handleSubmitPermissions)}>
+
+                <div className='flex flex-row justify-between items-center col-span-4'>
 
                     <span className='font-bold text-md'>تعیین دسترسی های کاربر</span>
 
@@ -101,21 +131,48 @@ export const ChangeUserPermissions = ({ userId, userRoleId }: { userId: number, 
                     </label>
 
                 </div>
-                {data?.data.map((item, index) => <label htmlFor={item.id.toString()} className={`cursor-pointer flex flex-row gap-2 justify-center items-center p-2 border-2 rounded text-center hover:bg-gray-50 ${false ? 'bg-blue-50 border-mint-green' : ''}`}>
+
+                {/* {data?.data.reduce<Array<{title:string , items : PermissionType[]}>>((pv, cv) => [] , []) } */}
+
+                {proccessedData?.map(cat => <div className='flex flex-col gap-1 col-span-2 lg:col-span-1 border rounded p-1'>
+
+                    <div className='flex flex-row gap-2 items-center justify-between'>
+                        <span className='text-body-2-bolder'>{captilizeFirstLetter(cat.title)}</span>
+
+                        <label className='flex flex-row gap-1 items-center border p-1 rounded cursor-pointer' htmlFor={`all-${cat.title}`}>
+                            <input id={`all-${cat.title}`} type='checkbox' checked={cat.items.every(i => currentSelected[i.id])} onChange={({target:{checked}})=>handleToggleCatSelect( cat.title ,checked)} />
+                            <span>انتخاب همه</span>
+
+                        </label>
+                    </div>
+
+                    {cat.items.map((item, index) => <label htmlFor={item.id.toString()} className={`cursor-pointer flex flex-row gap-2 justify-center items-center p-2 border-2 rounded text-center hover:bg-gray-50 ${false ? 'bg-blue-50 border-mint-green' : ''}`}>
+
+                        <input type='checkbox' id={item.id.toString()} key={item.id} {...register(`${item.id}`)} />
+
+                        <span>{captilizeFirstLetter(item.title)}</span>
+
+                    </label>)}</div>)}
+
+                {/* {data?.data.map((item, index) => <label htmlFor={item.id.toString()} className={`cursor-pointer flex flex-row gap-2 justify-center items-center p-2 border-2 rounded text-center hover:bg-gray-50 ${false ? 'bg-blue-50 border-mint-green' : ''}`}>
 
                     <input type='checkbox' id={item.id.toString()} key={item.id} {...register(`${item.id}`)} />
 
                     <span>{captilizeFirstLetter(item.title)}</span>
 
-                </label>)}
+                </label>)} */}
 
                 {isLoading && <Spinner />}
+                <div className='col-span-4 flex'>
 
-                <Button loading={mutateLoading}>ثبت تغییرات</Button>
 
+                <Button loading={mutateLoading} fullWidth>ثبت تغییرات</Button>
+
+                </div>
             </form>
+        }
 
-        return <div className='flex flex-col gap-2 items-center max-h-[80vh] overflow-auto w-full min-w-[80vw]'>
+        return <div className='flex flex-col gap-2 items-center max-h-[80vh] overflow-auto w-full min-w-[90vw]'>
             {Array.from(new Array(10)).map((i, index) => <div className='bg-gray-50 animate-pulse w-full h-8 '></div>)}
         </div>
 
