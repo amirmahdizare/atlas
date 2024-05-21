205e4cae2ee3799d67dd15c7702a211f4b13b533
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-21 13:16:06 +0330

    Add Welcome

--- app/(panel)/panel/ClientPage.tsx
+++ app/(panel)/panel/ClientPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import fullLogo from 'images/logo-full.svg'
+import Image from 'next/image'
+import { Divider } from '@components'
 export const ClientPage = () => {
   return (
-    <div>به پنل خوش آمدید</div>
+
+    <div className='flex flex-col gap-2 justify-center items-center h-60 border rounded p-2'>
+      <div className='font-bold text-h5-bolder'>به پنل اطلس خوش آمدید</div>
+      <Divider/>
+      <Image src={fullLogo} alt='لوگوی دپارتمان املاک اطلس'  width={100} height={50} />
+    </div>
   )
 }
