c5b87b4dac50a87180019aca71921efdcefc6d86
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-05-21 13:36:48 +0330

    Edit Admin Info

--- app/(panel)/panel/profile/ClientPage.tsx
+++ app/(panel)/panel/profile/ClientPage.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Button, Input } from '@components'
 import { IconPhoto, IconUser } from '@tabler/icons-react'
@@ -9,60 +9,99 @@ import { useCustomMutation, useUserInfo } from '@hooks'
 import { api } from '_api/config'
 import { UsersEndpoints } from '_api/endpoints/users'
 import { toast } from 'react-toastify'
-import { createFormData } from 'utils'
+import { createFormData, createMediaUrl } from 'utils'
 
 export const ClientPage = () => {
 
-    const { register, formState: { errors }, handleSubmit } = useForm<UserInfoType<File[]>>()
+    const { register, formState: { errors }, handleSubmit, reset, watch } = useForm<UserInfoType<File[] | string>>()
 
-    //TODO User Id Must Fetch From User Info 
-    // const { data , isLoading  } = useUserInfo()
+    const { data, isError, refetch } = useUserInfo()
 
     const { mutate, isLoading } = useCustomMutation({
         mutationFn: (data) => api.patch(UsersEndpoints.SINGLE_USER('24'), createFormData(data)),
         onSuccess: () => {
             toast.success('مشخصات شما با موفقیت ویرایش شد.')
+            refetch()
         },
         onError: () => {
             toast.error('خطا در ویرایش مشخصات شما.')
         }
     })
 
+    useEffect(() => {
+        if (typeof data?.data == 'object') {
+            const { firstName, lastName, userName, avatar } = data.data
+            reset({ firstName, userName, lastName, avatar })
+        }
+    }, [data?.data])
 
-    const mutateUserInfo = (data: UserInfoType<File[]>) => {
-        mutate({ ...data, avatar: data.avatar?.[0] })
+
+    const mutateUserInfo = (data: UserInfoType<File[] | string>) => {
+        mutate({ ...data, ...(typeof data.avatar?.[0] == 'object' ? ({ avatar: data.avatar?.[0] }) : ({})) })
     }
 
-    return (
-        <form className='flex flex-col gap-2' onSubmit={handleSubmit(mutateUserInfo)}>
-            <div className='flex flex-row gap-2 justify-between '>
+    const currentFormAvatar = watch('avatar')
 
-                <div className='flex flex-row gap-1 items-center'>
-                    <IconUser width={25} height={25} className='text-french-gray' />
-                    <span>ویرایش اطلاعات کاربری</span>
+    const renderAvatarUrl = () => {
+        if (typeof currentFormAvatar == 'string')
+            return createMediaUrl(currentFormAvatar)
+        else if (Array.isArray(currentFormAvatar) && typeof currentFormAvatar[0] == 'object') {
+            return URL.createObjectURL(currentFormAvatar[0])
+        }
+        else
+            return undefined
+    }
+
+    if (isError)
+        return <div></div>
+
+    else if (data?.data)
+
+        return (
+            <form className='flex flex-col gap-2' onSubmit={handleSubmit(mutateUserInfo)}>
+                <div className='flex flex-row gap-2 justify-between '>
+
+                    <div className='flex flex-row gap-1 items-center'>
+                        <IconUser width={25} height={25} className='text-french-gray' />
+                        <span>ویرایش اطلاعات کاربری</span>
+                    </div>
+
                 </div>
+                <Input
+                    label='نام'
+                    fullWidth
+                    register={register('firstName')} />
 
-            </div>
-            <Input
-                label='نام'
-                fullWidth
-                register={register('firstName')} />
+                <Input
+                    label='نام خانوادگی'
+                    fullWidth
+                    register={register('lastName', { required: { value: true, message: 'نام خانوادگی ضروری می باشد.' } })}
+                    error={!!errors.lastName}
+                    errorText={errors.lastName?.message}
+                />
 
-            <Input
-                label='نام خانوادگی'
-                fullWidth
-                register={register('lastName', { required: { value: true, message: 'نام خانوادگی ضروری می باشد.' } })}
-                error={!!errors.lastName}
-                errorText={errors.lastName?.message}
-            />
+                <Input
+                    label='یوزر نیم'
+                    fullWidth
+                    register={register('userName', { required: { value: true, message: 'یوزر نیم ضروری می باشد.' } })}
+                    error={!!errors.userName}
+                    errorText={errors.userName?.message}
+                />
 
-            <label htmlFor={'avatar'} className='flex flex-row gap-2 items-center cursor-pointer'>
-                <IconPhoto />
-                <span>عکس پروفایل</span>
-                <input type='file' id={'avatar'}  {...register('avatar')} />
-            </label>
 
-            <Button loading={isLoading}>تایید نهایی</Button>
-        </form>
-    )
+                <label htmlFor={'avatar'} className='flex flex-row gap-2 items-center cursor-pointer'>
+                    {!!renderAvatarUrl() ? <img src={renderAvatarUrl()} className='w-4 h-4 rounded-circle' /> : <IconPhoto className='w-4 h-4 ' />}
+
+                    <span>عکس پروفایل</span>
+                    <input type='file' id={'avatar'}  {...register('avatar')} />
+                </label>
+
+                <Button loading={isLoading}>تایید نهایی</Button>
+            </form>
+        )
+
+    return <div className='flex flex-col gap-2 items-center justify-center'>
+
+        {Array.from(new Array(10)).map((f, index) => <div key={index} className='bg-gray-50 animate-pulse w-full h-6'></div>)}
+    </div>
 }
