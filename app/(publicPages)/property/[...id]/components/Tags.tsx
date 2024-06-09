5c565e28615e88863658418764d0efc8a89f387a
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-09 13:14:18 +0330

    Add Tags

--- app/(publicPages)/property/[...id]/components/Tags.tsx
+++ app/(publicPages)/property/[...id]/components/Tags.tsx
@@ -13,10 +13,10 @@ export const Tags = ({ data: { tags } }: { data: PropertyDetailType }) => {
                 <span className='text-space-codet font-semibold text-h6-bolder'>برچسب ها</span>
 
                 <div className='flex flex-row gap-1 items-center'>
-
-                    {tags?.map(tag => <Link target='_blank' href={`/s/tag=[${tag.name}]`} style={{ backgroundColor: tag.backgrondColor, color: tag.textColor }} className='text-body-2-normal p-1 rounded'>
+                    {/* target='_blank' href={`/s/tag=[${tag.name}]`} */}
+                    {tags?.map(tag => <div style={{ backgroundColor: tag.backgrondColor, color: tag.textColor }} className='text-body-3-normal p-1 rounded'>
                         {tag.name}
-                    </Link>)}
+                    </div>)}
 
                 </div>
 
