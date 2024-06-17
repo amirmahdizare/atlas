22ae353ca54cc4481518c1cfa5662ae5ccb33e6a
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-17 22:55:59 +0330

    Fix Login Page

--- app/(general)/login/ClientPage.tsx
+++ app/(general)/login/ClientPage.tsx
@@ -16,37 +16,39 @@ export const ClientPage = () => {
 
     const { data, isError } = useUserInfo()
 
-    if (data)
-        redirect('/')
-    else if (isError)
-    return (
-        <div className='container max-w-[1400px] mx-auto h-full pt-8 lg:pt-0'>
-            <div className='grid grid-cols-7 gap-1 lg:gap-4  h-full'>
+    console.log({data , isError})
 
-                <StickyMobileHeader />
+    if (!!data?.data?.id)
+        return redirect('/')
+    else if (!data?.data?.id)
+        return (
+            <div className='container max-w-[1400px] mx-auto h-full pt-8 lg:pt-0'>
+                <div className='grid grid-cols-7 gap-1 lg:gap-4  h-full'>
 
-                <div className='col-span-7 lg:col-span-4 flex flex-col items-center justify-center gap-3  p-2 relative'>
+                    <StickyMobileHeader />
 
-                    <div className='w-[133px] aspect-[2/1] relat ive absolute top-4 right-4 lg:block hidden'>
-                        <Image src={fullLogo} alt='لوگوی دپارتمان املاک اطلس' fill />
+                    <div className='col-span-7 lg:col-span-4 flex flex-col items-center justify-center gap-3  p-2 relative'>
+
+                        <div className='w-[133px] aspect-[2/1] relat ive absolute top-4 right-4 lg:block hidden'>
+                            <Image src={fullLogo} alt='لوگوی دپارتمان املاک اطلس' fill />
+                        </div>
+
+                        <div className='min- w-2/3 lg:w-fit'>
+                            <DataForm />
+                        </div>
+
+                        <Link href={'/'} className='hover:text-coral '>بازگشت به صفحه اصلی</Link>
                     </div>
 
-                    <div className='min- w-2/3 lg:w-fit'>
-                        <DataForm />
-                    </div>
 
-                    <Link href={'/'} className='hover:text-coral '>بازگشت به صفحه اصلی</Link>
+
+                    <SideBox />
+
+
                 </div>
 
-
-
-                <SideBox />
-
-
             </div>
-
-        </div>
-    )
+        )
 
     return <div className='h-full w-full rounded animate-pulse bg-gray-50 flex flex-row justify-center items-center'>
         <Spinner />
