b8d7a3755e8cce98b5fa69d848c2aefcb784b951
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-19 15:21:37 +0330

    Implement News

--- app/components/landingPage/News/News.tsx
+++ app/components/landingPage/News/News.tsx
@@ -3,55 +3,75 @@ import { Announcement, Building } from '@icons'
 import { IconArrowDownLeft } from '@tabler/icons-react'
 import { Slider } from './components/Slider/Slider'
 import Link from 'next/link'
+import { ssrMutate } from '_api/serverSideConfig'
+import { PropretyEndPoints } from '_api/endpoints/property'
+import { PropertyDetailType } from 'types'
 
-export const News = () => {
-    return (
-        <div className='flex flex-col gap-6 items-stretch'>
-            <div className='flex flex-row gap-4 justify-between lg:justify-center'>
-
-                <div className='text-raisin-black text-h3-bolder relative text-center'>
-                    آگهی های جدید
-                    <div className='absolute bg-mint-green w-[80px] left-1/2 -translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
-                </div>
+export const News = async () => {
 
 
+    try {
 
-                <Link className='flex flex-row gap-1 cursor-pointer hover:text-coral lg:hidden' href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'}>
-                    <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
-                    <IconArrowDownLeft width={15} height={15} />
-                </Link>
-            </div>
+        const news = await ssrMutate<PropertyDetailType[]>({
+            path: PropretyEndPoints.SEARCH,
+            body: { limit: 15, page: 1 }
+        })
 
-            <div className='flex flex-col p-2 lg:p-4 bg-seasalt gap-2 overflow-hidden'>
-                <div className='flex flex-row gap-2 justify-between items-center'>
+        return (
+            <div className='flex flex-col gap-6 items-stretch'>
+                <div className='flex flex-row gap-4 justify-between lg:justify-center'>
 
-                    <div className='flex flex-row gap-2 items-center'>
-                        <Building width={25} height={25} className='text-raisin-black' />
-                        <p className='text-h6-bolder text-ellipsis line-clamp-1'>
-                            <span className='text-mint-green'>فروش و رهن و اجاره</span>
-                            &nbsp;
-                            مسکونی، تجاری و اداری
-                        </p>
-
-                        <div className='bg-anti-flash-white-lighter p-1 text-body-3-normal  flex-row  gap-1 items-center hidden lg:flex'>
-                            <Announcement className='text-ultra-violet' width={20} height={20} />
-                            <span className='text-body-3-normal'>+58 آگهی</span>
-                        </div>
-
+                    <div className='text-raisin-black text-h3-bolder relative text-center'>
+                        آگهی های جدید
+                        <div className='absolute bg-mint-green w-[80px] left-1/2 -translate-x-1/2 h-[2px] top-full mt-1.5 rounded '></div>
                     </div>
 
-                    <Link href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'} className=' flex-row gap-1 cursor-pointer hover:text-coral hidden lg:flex'>
+
+
+                    <Link className='flex flex-row gap-1 cursor-pointer hover:text-coral lg:hidden' href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'}>
                         <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
                         <IconArrowDownLeft width={15} height={15} />
                     </Link>
-
                 </div>
 
-                <Slider />
+                <div className='flex flex-col p-2 lg:p-4 bg-seasalt gap-2 overflow-hidden'>
+                    <div className='flex flex-row gap-2 justify-between items-center'>
+
+                        <div className='flex flex-row gap-2 items-center'>
+                            <Building width={25} height={25} className='text-raisin-black' />
+                            <p className='text-h6-bolder text-ellipsis line-clamp-1'>
+                                <span className='text-mint-green'>فروش و رهن و اجاره</span>
+                                &nbsp;
+                                مسکونی، تجاری و اداری
+                            </p>
+
+                            {news?.length > 15 && <div className='bg-anti-flash-white-lighter p-1 text-body-3-normal  flex-row  gap-1 items-center hidden lg:flex'>
+                                <Announcement className='text-ultra-violet' width={20} height={20} />
+                                <span className='text-body-3-normal'>+15 آگهی</span>
+                            </div>}
+
+                        </div>
+
+                        <Link href={'/s/hashtgerd-newcity/شهر جدید هشتگرد'} className=' flex-row gap-1 cursor-pointer hover:text-coral hidden lg:flex'>
+                            <span className='text-mint-green text-body-2-bolder hover:text-coral'>همه آگهی ها</span>
+                            <IconArrowDownLeft width={15} height={15} />
+                        </Link>
+
+                    </div>
+
+                    <Slider data={news} />
+
+                    <ul className='sr-only'>
+                        {news.map(item => <a href={`/proprrty/${item.id}/${item.title}`}>{item.title} | دپارتمان املاک اطلس</a>)}
+                    </ul>
+                </div>
+
+
+
             </div>
+        )
+    } catch (error) {
 
-
-
-        </div>
-    )
+        return <></>
+    }
 }
--- app/components/landingPage/News/components/Slider/Slider.tsx
+++ app/components/landingPage/News/components/Slider/Slider.tsx
@@ -1,56 +1,62 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import {  news } from '../../data.mock';
+import { news } from '../../data.mock';
 
 import { PropertyListCard } from '@components';
+import { PropertyDetailType } from 'types';
 
-export const Slider = () => {
-  return (
-    <div className='w-[calc(100%+80px)]'>
+export const Slider = ({ data }: { data: PropertyDetailType[] }) => {
 
-      <Swiper
-        // spaceBetween={30}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 5
-          },
-          300:{
-            slidesPerView: 2,
-            spaceBetween: 5
-          },
-          470: {
-            slidesPerView: 3,
-            spaceBetween: 10
-          },
-          700: {
-            slidesPerView: 4,
-            spaceBetween: 10
-          },
 
-          900: {
-            slidesPerView: 5,
-            spaceBetween: 10
-          },
+  if (data.length == 0)
+    return <span>در حال حاضر ملکی وجود ندارد.</span>
+  else if (data.length > 0)
+    return (
+      <div className='w-[calc(100%+80px)]'>
 
-          1400: {
-            slidesPerView: 5,
-            spaceBetween: 10
-          },
-          // 1800: {
-          //   slidesPerView: 7,
-          //   spaceBetween: 10
-          // },
+        <Swiper
+          // spaceBetween={30}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 5
+            },
+            300: {
+              slidesPerView: 2,
+              spaceBetween: 5
+            },
+            470: {
+              slidesPerView: 3,
+              spaceBetween: 10
+            },
+            700: {
+              slidesPerView: 4,
+              spaceBetween: 10
+            },
 
-        }}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        className='flex flex-col items-stretch justify-stretch'
-      >
-        {news.map(item => <SwiperSlide className='h-full'><PropertyListCard key={item.id} {...item}/></SwiperSlide>)}
+            900: {
+              slidesPerView: 5,
+              spaceBetween: 10
+            },
 
-      </Swiper>
-    </div>
-  );
+            1400: {
+              slidesPerView: 5,
+              spaceBetween: 10
+            },
+            // 1800: {
+            //   slidesPerView: 7,
+            //   spaceBetween: 10
+            // },
+
+          }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className='flex flex-col items-stretch justify-stretch'
+        >
+          {data.map(item => <SwiperSlide className='h-full'><PropertyListCard key={item.id} {...item} /></SwiperSlide>)}
+
+        </Swiper>
+      </div>
+    );
 };
