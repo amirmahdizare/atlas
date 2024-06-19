28fd7d784ab57c85f83efc08f5dab89b94f85a39
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-19 15:08:35 +0330

    Implement Suggested

--- app/_api/serverSideConfig.tsx
+++ app/_api/serverSideConfig.tsx
@@ -1,1 +1,36 @@
+import { cookies } from "next/headers"
+
 export const ApiBaseURL = process.env.NEXT_PUBLIC_API
+
+
+
+
+
+export const ssrMutate = async <T,>({ method = 'POST', path, body }: { path: string, body?: any, method?: 'POST' | 'PUT' }) => {
+
+    try {
+
+
+        const cookiesValue = cookies()
+
+        const response = await fetch((ApiBaseURL ?? '')?.concat(path), {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `bearer ${cookiesValue.get('access_token')?.value ?? ''}`
+            },
+            body: JSON.stringify(body),
+
+        })
+
+        const data: T = await response.json()
+
+
+        return data
+
+    }
+    catch (error) {
+        return Promise.reject(error)
+    }
+
+}
--- app/components/landingPage/HeroSection/components/Suggest/Suggest.tsx
+++ app/components/landingPage/HeroSection/components/Suggest/Suggest.tsx
@@ -4,23 +4,23 @@ import React from 'react'
 import photo from 'images/landingLeft.svg'
 import Image from 'next/image'
 import Link from 'next/link'
-import { ApiBaseURL } from '_api/serverSideConfig'
+import {  ssrMutate } from '_api/serverSideConfig'
 import { PropretyEndPoints } from '_api/endpoints/property'
 import { PropertyDetailType } from 'types'
 import { createMediaUrl, isFileSrcImage } from 'utils'
 
 
-const tempImages = [
-    { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'ویلای 200 متری مهستان کاسبی ' },
-    { img: 'https://www.soorban.com/images/news/2023/09/1694333140_P5dC4.jpg', title: 'آپارتمان 95 متری نبرد' },
-    { img: 'https://www.gfxdownload.ir/uploads/posts/2023-11/house1.jpg', title: 'کلنگی مهستان' },
-    { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'ویلای و خوابه کاسبی ' },
-    { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'سوبلکس' }
+// const tempImages = [
+//     { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'ویلای 200 متری مهستان کاسبی ' },
+//     { img: 'https://www.soorban.com/images/news/2023/09/1694333140_P5dC4.jpg', title: 'آپارتمان 95 متری نبرد' },
+//     { img: 'https://www.gfxdownload.ir/uploads/posts/2023-11/house1.jpg', title: 'کلنگی مهستان' },
+//     { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'ویلای و خوابه کاسبی ' },
+//     { img: 'https://www.khanetarh.com/wp-content/uploads/2018/01/Design-a-duplex-villa-1.webp', title: 'سوبلکس' }
 
 
-]
+// ]
 
-const Item = ({ img, title , id }: { img: string, title: string  ,id:string}) => <Link href={`/property/${id}/${title}`} className='flex flex-col gap-4 lg:m-0.5 items-end justify-end brightne relative ss-75 rounded aspect-square object-scale-down w-[120px] lg:w-[140px] shrink-0 ' style={{ backgroundImage: `url(${img})` }}>
+const Item = ({ img, title, id }: { img: string, title: string, id: string }) => <Link href={`/property/${id}/${title}`} className='flex flex-col gap-4 lg:m-0.5 items-end justify-end brightne relative ss-75 rounded aspect-square object-scale-down w-[120px] lg:w-[140px] shrink-0 ' style={{ backgroundImage: `url(${img})` }}>
     <Image src={img} alt='j' fill className='rounded-lg aspect-square' />
     <span className='text-body-3-bolder text-white  text-ellipsis overflo w-hidden whitespace-pre-wrap backdrop-brightness-50 leading-3 p-0.5 w-full text-center  h-8 align-middle flex flex-row justify-center items-center'>{title}</span>
 </Link>
@@ -29,13 +29,13 @@ const Item = ({ img, title , id }: { img: string, title: string  ,id:string}) =>
 export const Suggest = async () => {
 
     try {
-        const response = await fetch((ApiBaseURL ?? '')?.concat(PropretyEndPoints.SEARCH), { method: 'POST' ,   headers: {
-            "Content-Type": "application/json",
-          }, body: JSON.stringify({ isSuggested: true, page: 1, limit: 10 }) })
 
-        const data: Array<PropertyDetailType> = await response.json()
+        const data = await ssrMutate<Array<PropertyDetailType>>({
+            path: PropretyEndPoints.SEARCH,
+            body: { isSuggested: true, page: 1, limit: 10 }
+        })
 
-        if (data.length == 0)
+        if (data?.length == 0)
             return <></>
 
         return (
@@ -50,7 +50,7 @@ export const Suggest = async () => {
                 </div>
 
                 <div className='flex flex-row col-span-3 lg:col-span-4 gap-2 text-white overflow-auto '>
-                    {data.map(item => <Item id={item.id} img={createMediaUrl(item.medias?.find(i=>isFileSrcImage(i)))} title={item.title} />)}
+                    {data.map(item => <Item id={item.id} img={createMediaUrl(item.medias?.find(i => isFileSrcImage(i)))} title={item.title} />)}
 
                     {/* <ul>
                         {data.map(i=>)}
