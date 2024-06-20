e892bde8c00a1a4e82572244ab29612c421de301
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-20 16:29:40 +0330

    Fix stroke-width

--- app/icons/Bookmark.tsx
+++ app/icons/Bookmark.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Bookmark = (props:React.SVGProps<SVGSVGElement>) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" viewBox="0 0 16 20" fill="none" {...props}>
-            <path d="M1 4.2C1 3.07989 1 2.51984 1.21799 2.09202C1.40973 1.71569 1.71569 1.40973 2.09202 1.21799C2.51984 1 3.07989 1 4.2 1H11.8C12.9201 1 13.4802 1 13.908 1.21799C14.2843 1.40973 14.5903 1.71569 14.782 2.09202C15 2.51984 15 3.07989 15 4.2V19L8 14L1 19V4.2Z" stroke="currentColor" stroke-width="2" stroke-linejoin="round" />
+            <path d="M1 4.2C1 3.07989 1 2.51984 1.21799 2.09202C1.40973 1.71569 1.71569 1.40973 2.09202 1.21799C2.51984 1 3.07989 1 4.2 1H11.8C12.9201 1 13.4802 1 13.908 1.21799C14.2843 1.40973 14.5903 1.71569 14.782 2.09202C15 2.51984 15 3.07989 15 4.2V19L8 14L1 19V4.2Z" stroke="currentColor" strokeWidth="2" stroke-linejoin="round" />
         </svg>
     )
 }
--- app/icons/ContactInfo.tsx
+++ app/icons/ContactInfo.tsx
@@ -4,12 +4,12 @@ export const ContactInfo = (props: React.SVGProps<SVGSVGElement>) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
             <g clip-path="url(#clip0_2130_1565)">
-                <path d="M20.0352 6V18C20.0352 18.5304 19.8244 19.0391 19.4494 19.4142C19.0743 19.7893 18.5656 20 18.0352 20H8.03516C7.50472 20 6.99602 19.7893 6.62094 19.4142C6.24587 19.0391 6.03516 18.5304 6.03516 18V6C6.03516 5.46957 6.24587 4.96086 6.62094 4.58579C6.99602 4.21071 7.50472 4 8.03516 4H18.0352C18.5656 4 19.0743 4.21071 19.4494 4.58579C19.8244 4.96086 20.0352 5.46957 20.0352 6Z" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
-                <path d="M10.0352 16H16.0352" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
-                <path d="M11.0352 11C11.0352 11.5304 11.2459 12.0391 11.6209 12.4142C11.996 12.7893 12.5047 13 13.0352 13C13.5656 13 14.0743 12.7893 14.4494 12.4142C14.8244 12.0391 15.0352 11.5304 15.0352 11C15.0352 10.4696 14.8244 9.96086 14.4494 9.58579C14.0743 9.21071 13.5656 9 13.0352 9C12.5047 9 11.996 9.21071 11.6209 9.58579C11.2459 9.96086 11.0352 10.4696 11.0352 11Z" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
-                <path d="M4.03516 8H7.03516" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
-                <path d="M4.03516 12H7.03516" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
-                <path d="M4.03516 16H7.03516" stroke="currentColor" stroke-width="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M20.0352 6V18C20.0352 18.5304 19.8244 19.0391 19.4494 19.4142C19.0743 19.7893 18.5656 20 18.0352 20H8.03516C7.50472 20 6.99602 19.7893 6.62094 19.4142C6.24587 19.0391 6.03516 18.5304 6.03516 18V6C6.03516 5.46957 6.24587 4.96086 6.62094 4.58579C6.99602 4.21071 7.50472 4 8.03516 4H18.0352C18.5656 4 19.0743 4.21071 19.4494 4.58579C19.8244 4.96086 20.0352 5.46957 20.0352 6Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M10.0352 16H16.0352" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M11.0352 11C11.0352 11.5304 11.2459 12.0391 11.6209 12.4142C11.996 12.7893 12.5047 13 13.0352 13C13.5656 13 14.0743 12.7893 14.4494 12.4142C14.8244 12.0391 15.0352 11.5304 15.0352 11C15.0352 10.4696 14.8244 9.96086 14.4494 9.58579C14.0743 9.21071 13.5656 9 13.0352 9C12.5047 9 11.996 9.21071 11.6209 9.58579C11.2459 9.96086 11.0352 10.4696 11.0352 11Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M4.03516 8H7.03516" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M4.03516 12H7.03516" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
+                <path d="M4.03516 16H7.03516" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" stroke-linejoin="round" />
             </g>
             <defs>
                 <clipPath id="clip0_2130_1565">
