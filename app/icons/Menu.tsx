aba7623a09deb47967c7c0aa42ec5bd265340ed9
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-20 16:28:56 +0330

    Fix issue

--- app/icons/Menu.tsx
+++ app/icons/Menu.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Menu = (props: React.SVGProps<SVGSVGElement>) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 30 26" fill="none" {...props}>
-            <path d="M1.66602 2.33301H14.9993M1.66602 12.9997H28.3327M14.9993 23.6663H28.3327" stroke="#28303F" strokeWidth="3" stroke-linecap="round" />
+            <path d="M1.66602 2.33301H14.9993M1.66602 12.9997H28.3327M14.9993 23.6663H28.3327" stroke="#28303F" strokeWidth="3" strokeLinejoin="round" />
         </svg>
     )
 }
