8bab263abc2ea762249410bee229fab3ed902a8b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-20 17:44:20 +0330

    Add Location Search

--- app/components/DesktopHeader/DesktopHeader.tsx
+++ app/components/DesktopHeader/DesktopHeader.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { UserStatus } from './components/UserStatus/UserStatus'
 import { ActionButtons } from './components/ActionButtons/ActionButtons'
 import { usePathname } from 'next/navigation'
+import { useCities } from '@hooks'
 // import { ActionButtons } from './components/ActionButtons/ActionButtons'
 
 
@@ -26,6 +27,8 @@ const LinkComponent = ({ title, link }: { title: string, link: string }) => {
     return <Link href={link} className='hover:text-coral'>{title}</Link>
 }
 export const DesktopHeader = () => {
+
+    const { data , isLoading} = useCities()
     return (
         <div className='lg:flex flex-row gap-4 items-center justify-between p-2 text-body-3-bolder text-ultra-violet hidden'>
             <Link href={'/'}><Image src={fullLogo} alt='لوگوی دپارتمان اطلس' className='h-5' /></Link>
@@ -38,22 +41,27 @@ export const DesktopHeader = () => {
                     <div className='w-3/4 bg-robin-egg-lighter h-[1px] top-full mt-1 right-1/2 translate-x-1/2 absolute'></div>
                 </Link> */}
 
-                {/* <AccordionMenu
-                    title='خرید'
-                    items={[
-                        {
-                            link: '#',
-                            title: 'خرید آپارتمان در شهرجدید هشتگرد'
-                        },
-                        {
-                            link: '#',
-                            title: 'خرید آپارتمان در هشتگرد'
-                        },
-                        {
-                            link: '#',
-                            title: 'خرید ویلا در هشتگرد'
-                        }
-                    ]} /> */}
+                <AccordionMenu
+                    title='جستجوی ملک'
+                    href='/s/all'
+                    items={
+                        data?.data.map(item => ({link:`/s/${item.name}-city` , title:`جستجوی ملک در ${item.faTitle}`})) ?? []
+                    //     [
+                    //     {
+                    //         link: '#',
+                    //         title: 'خرید آپارتمان در شهرجدید هشتگرد'
+                    //     },
+                    //     {
+                    //         link: '#',
+                    //         title: 'خرید آپارتمان در هشتگرد'
+                    //     },
+                    //     {
+                    //         link: '#',
+                    //         title: 'خرید ویلا در هشتگرد'
+                    //     }
+                    // ]
+                    
+                    } />
 
 
                 {/* <AccordionMenu
@@ -72,9 +80,9 @@ export const DesktopHeader = () => {
                             title: 'اجاره ویلا در هشتگرد'
                         }
                     ]} /> */}
-                    {/* //Todo Search Property For All Cities and SubCities */}
+                {/* //Todo Search Property For All Cities and SubCities */}
 
-                <LinkComponent link='/s/all' title='جستجوی ملک' />
+                {/* <LinkComponent link='/s/all' title='جستجوی ملک' /> */}
                 <LinkComponent link='/blogs' title='اخبار و مقالات' />
                 <LinkComponent link='/aboutus' title='درباره ما' />
                 <LinkComponent link='/contactus' title='تماس با ما' />
--- app/components/DesktopHeader/components/AccordionMenu/AccordionMenu.tsx
+++ app/components/DesktopHeader/components/AccordionMenu/AccordionMenu.tsx
@@ -3,25 +3,25 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import { IconArrowDown, IconChevronDown } from '@tabler/icons-react'
 
-export const AccordionMenu = ({ items, title }: { title: string, items: Array<{ title: string, link: string }> }) => {
+export const AccordionMenu = ({ items, title, href }: { title: string, items: Array<{ title: string, link: string }>, href?: string }) => {
 
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
     return (
-        <div className='flex flex-row gap-0.5 items-center text-ultra-violet relative cursor-pointer '
+        <Link href={href ?? '#'} className='flex flex-row gap-0.5 items-center text-ultra-violet relative cursor-pointer '
             onMouseEnter={() => setIsOpen(true)}
             onMouseLeave={() => setIsOpen(false)}
-            // onMouseOut={() => setIsOpen(false)}
+        // onMouseOut={() => setIsOpen(false)}
         >
             <span className='hover:text-coral'>{title}</span>
 
             <IconChevronDown width={15} height={15} className={isOpen ? 'rotate-180 transition-all duration-300' : ' transition-all duration-300'} />
 
-            <div className={`absolute flex flex-col gap-2 p-2 rounded shadow bg-white text-body-3-normal top-full mt-1 right-1/2 border translate-x-1/2 whitespace-nowrap transition-all duration-300 ${isOpen ? 'opacity-1' : ' opacity-0'}`}>
+            <div className={`absolute flex flex-col gap-2 p-2 rounded shadow bg-white text-body-3-normal top-full mt-1 right-1/2 border translate-x-1/2 whitespace-nowrap transition-all duration-300 z-20 ${isOpen ? 'opacity-1' : ' opacity-0'}`}>
                 {items.map(item => <Link key={item.link} className='hover:text-coral' href={item.link}>{item.title}</Link>)}
             </div>
 
-        </div>
+        </Link>
     )
 
 }
