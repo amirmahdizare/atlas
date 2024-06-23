562c8c0ef65d7a033f02c18cbdb5bd4323f398e6
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-06-23 19:07:49 +0330

    Fix Modal Overlap

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -17,8 +17,8 @@ export const Modal = ({ open, setOpen, children, fitWidth, fitHeight }: IProps) 
         return (
             <div className='fixed h-full w-full z-30 backdrop-blur backdrop-brightness-90  top-0 left-0 rounded   flex flex-row justify-center items-center'>
                 <ClickAwayListener onClickAway={() => setOpen(false)}>
-                    <div className={`z-10  lg:rounded-sm bg-white shadow p-1 overflow-auto ${fitHeight ? '' : 'lg:h-[660px] h-full'}  ${fitWidth ? '' : 'lg:w-[360px] w-full'}`}>
-                        {children}
+                    <div className={`z-10  lg:rounded-sm bg- white shadow p-1 max-h-[calc(100vh-10%)] overflow-auto bg-white ${fitHeight ? '' : 'lg:h-[660px] h-full'}  ${fitWidth ? '' : 'lg:w-[360px] w-full'}`}>
+                        {children} 
                     </div>
                 </ClickAwayListener>
             </div>
