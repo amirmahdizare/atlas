34b8e09b304d7e2742bd567f83ea5fd0d47e7eff
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-07-02 15:57:35 +0330

    Fix Agent Page

--- app/(publicPages)/agent/[...info]/components/News/components/Properties.tsx
+++ app/(publicPages)/agent/[...info]/components/News/components/Properties.tsx
@@ -6,15 +6,15 @@ import { news } from '../data.mock';
 import { PropertyListCard } from '@components';
 import { UserFullInfo } from 'types';
 
-export const Properties = ({data}:{data:UserFullInfo}) => {
+export const Properties = ({ data }: { data: UserFullInfo }) => {
 
-    if (data.products?.length == 0)
+    if (data.products.filter(i => !!i.active)?.length == 0)
         return <span className='text-center'>مشاور آگهی فعالی ندارد</span>
 
     return (
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1'>
 
-            {data.products?.map(item => <div className='col-span-1 lg:col-span-1'>
+            {data.products?.filter(i => !!i.active).map(item => <div className='col-span-1 lg:col-span-1'>
                 <PropertyListCard key={item.id} {...item} />
             </div>)}
 
