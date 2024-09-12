07946e4822f8012eba9ec2cdb86d0c7e60a5f3b1
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-09-12 15:48:04 +0330

    Add Copy

--- app/(publicPages)/property/[...id]/components/AgentInfo/components/ActionButton.tsx
+++ app/(publicPages)/property/[...id]/components/AgentInfo/components/ActionButton.tsx
@@ -1,15 +1,23 @@
 'use client'
 import { Button } from '@components'
-import { IconMessage, IconPhone } from '@tabler/icons-react'
+import { IconCopy, IconMessage, IconPhone } from '@tabler/icons-react'
 import Link from 'next/link'
 import React from 'react'
-import { createPhoneCallLink } from 'utils'
+import { toast } from 'react-toastify'
+import { copyLink, createPhoneCallLink } from 'utils'
 
 export const ActionButton = ({ phoneNumber }: { phoneNumber: string }) => {
     return (
         <div className='flex flex-row gap-1 items-center'>
             <Link href={`sms:+${phoneNumber}?&body=سلام وقت بخیر امکان بازدید وجود داره؟`} className='lg:hidden'> <span className='sr-only'>پیامک به مشاور</span> <Button icon={IconMessage} bgColor='white' textColor='textGray' ></Button></Link>
-            <Link href={createPhoneCallLink(phoneNumber)}><Button icon={IconPhone} bgColor='primaryNormal'>تماس با مشاور</Button></Link>
+            <div className='flex flex-row gap-1.5 items-center'>
+                <Link href={createPhoneCallLink(phoneNumber)}><Button icon={IconPhone} bgColor='primaryNormal'>تماس با مشاور</Button></Link>
+                <IconCopy className='cursor-pointer text-gray-400 hidden lg:flex hover:text-gray-500' onClick={() => {
+                    copyLink(phoneNumber)
+                    toast.success('شماره مشاور با موفقیت کپی شد')
+                }} />
+
+            </div>
         </div>
 
     )
